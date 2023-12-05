ffe2fb5f4e566864c0c2110e5321a75d68d373e0
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-05 17:20:10 -0400

    Add navbar and copy edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import HomeCard from "@components/HomeCard";
 import styles from "./page.module.css";
 import Socials from "@components/Socials";
+import NavBar from "@components/NavBar";
 
 export default function Home() {
   return (
     <main>
+      {/* <NavBar /> */}
       <HomeCard>
         <h1>Ricardo Nazario</h1>
         <p className={styles.bio}>
-          Hello. I am Ricardo. A software developer who loves creating joyful
-          products that make life a little nicer for everyone.
+          Hello. I'm a software developer who loves creating joyful products
+          that make life a little nicer for everyone.
         </p>
-        <Socials />
       </HomeCard>
     </main>
   );
