97f9b77d4e1f8abf5af5f7a9b5bcbe7ff3ae6425
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-05 17:20:59 -0400

    created navbar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,17 @@
+import Socials from "@components/Socials";
+import Link from "next/link";
+import styles from "./styles.module.css";
+import R from "@icons/R";
+function NavBar() {
+  return (
+    <nav className={styles.container}>
+      <Link href="/">
+        <R height={48} width={48} />
+      </Link>
+
+      <Socials />
+    </nav>
+  );
+}
+
+export default NavBar;
