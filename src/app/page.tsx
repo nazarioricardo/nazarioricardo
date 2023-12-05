cf828e7a9bab3b4f82149f9c3e3a20fcf14151f6
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-05 17:31:52 -0400

    Copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HomeCard from "@components/HomeCard";
 import styles from "./page.module.css";
-import Socials from "@components/Socials";
 import NavBar from "@components/NavBar";
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
     <main>
       {/* <NavBar /> */}
       <HomeCard>
-        <h1>Ricardo Nazario</h1>
+        <h1 className={styles.title}>Ricardo Nazario</h1>
         <p className={styles.bio}>
           Hello. I'm a software developer who loves creating joyful products
           that make life a little nicer for everyone.
