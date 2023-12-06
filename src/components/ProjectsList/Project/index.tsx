31ed952e678440dc0dcbcdfaac1b82222a51b68f
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-06 18:28:02 -0400

    Added project

--- src/components/ProjectsList/Project/index.tsx
+++ src/components/ProjectsList/Project/index.tsx
@@ -1,6 +1,7 @@
 import Image, { StaticImageData } from "next/image";
 import Copper from "./new-copper.png";
 import styles from "./styles.module.css";
+import Link from "next/link";
 
 type ProjectProps = {
   src: StaticImageData;
@@ -10,12 +11,25 @@ type ProjectProps = {
 
 function Project() {
   return (
-    <div className={styles.project}>
+    <Link className={styles.project} href="https://www.copper.com/">
       <div className={styles.imageContainer}>
-        <Image src={Copper} alt={"test"} height={175} width={175} />
+        <Image
+          style={{ borderRadius: "50%", width: 174, height: 174 }}
+          objectFit="cover"
+          src={Copper}
+          alt={"test"}
+          height={174}
+          width={174}
+        />
       </div>
-      <h3>Project</h3>
-    </div>
+      <div className={styles.content}>
+        <h3>Project</h3>
+        <p>
+          I've worked as a full stack and mobile developer for the past 6 years,
+          making products in the music, medical, and energy industries.
+        </p>
+      </div>
+    </Link>
   );
 }
 
