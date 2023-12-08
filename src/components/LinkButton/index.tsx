ba185cbd63dc71cc778a6acf06fac5bdbbad7141
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-08 19:00:12 -0400

    Added link button

---
+++ src/components/LinkButton/index.tsx
@@ -0,0 +1,21 @@
+import Link, { LinkProps } from "next/link";
+import { HTMLProps, FC, RefAttributes } from "react";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { SvgIconTypeMap } from "@mui/material";
+import styles from "./styles.module.css";
+
+type LinkButtonProps = {
+  text: string;
+  Icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+} & LinkProps &
+  RefAttributes<HTMLAnchorElement>;
+
+function LinkButton({ text, Icon, href, ...props }: LinkButtonProps) {
+  return (
+    <Link {...props} href={href} download className={styles.button}>
+      {text} {Icon && <Icon />}
+    </Link>
+  );
+}
+
+export default LinkButton;
