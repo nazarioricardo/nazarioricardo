71575511ce61c56630ac7ab05a3cc217dc6b36a4
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-09 20:25:30 -0400

    Mobile site

--- src/components/ProjectsList/Project/index.tsx
+++ src/components/ProjectsList/Project/index.tsx
@@ -12,15 +12,24 @@ type ProjectProps = {
 
 function Project({ src, link, alt, title, description }: ProjectProps) {
   return (
-    <Link className={styles.project} href={link}>
-      <div className={styles.imageContainer}>
-        <Image className={styles.image} src={src} alt={alt} />
-      </div>
-      <div className={styles.content}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </Link>
+    <>
+      <Link className={styles.project} href={link}>
+        <div className={styles.imageContainer}>
+          <Image className={styles.image} src={src} alt={alt} />
+        </div>
+        <div className={styles.content}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </Link>
+      <Link className={styles.mobileProject} href={link}>
+        <div className={styles.mobileHeader}>
+          <Image className={styles.image} src={src} alt={alt} />
+          <h3>{title}</h3>
+        </div>
+        <p className={styles.description}>{description}</p>
+      </Link>
+    </>
   );
 }
 
