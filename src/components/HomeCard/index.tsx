5bf77f24f30451e2b4bf37841577382e5c44a098
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-11 14:56:51 -0400

    from below

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
-import Button from "@nr/components/Button";
 import styles from "./styles.module.css";
+import {
+  ExpandLess,
+  ExpandLessRounded,
+  ExpandMoreRounded,
+  ExpandRounded,
+} from "@mui/icons-material";
 
 type HomeCardProps = {
   children: ReactNode;
@@ -15,9 +20,16 @@ function HomeCard({ shouldDisplay, onClickBack, children }: HomeCardProps) {
         shouldDisplay ? styles.displayCard : null
       }`}
     >
-      <div className={styles.backButtonContainer}>
-        <Button onClick={onClickBack} text="Back" />
-      </div>
+      {shouldDisplay ? (
+        <button className={styles.header} onClick={onClickBack}>
+          About Me <ExpandMoreRounded fontSize="large" />
+        </button>
+      ) : (
+        <button className={styles.header} onClick={onClickBack}>
+          About Me <ExpandLessRounded fontSize="large" />
+        </button>
+      )}
+
       {children}
       <div className={styles.fullHeight} />
     </div>
