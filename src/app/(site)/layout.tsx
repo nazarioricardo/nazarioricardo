52849da6cb87e9a580a0b1c03a80993085ee3213
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-12 11:57:35 -0400

    Favicons

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { jomolhari, josefinSans, josefinSlab } from "./fonts";
 import "./globals.css";
 import NavBar from "@nr/components/NavBar";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Ricardo Nazario",
@@ -18,6 +19,29 @@ export default function RootLayout({
       className={`${jomolhari.variable} ${josefinSans.variable} ${josefinSlab.variable}`}
       lang="en"
     >
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d1d1d" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#1d1d1d" />
+      </Head>
       <body className={josefinSlab.className}>
         <NavBar />
         {children}
