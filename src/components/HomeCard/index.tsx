35df15047eae99a5d88a30005c742c960b5eb0d0
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-14 16:51:41 -0400

    Simpler styles (#11)

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -16,30 +16,37 @@ function HomeCard() {
           {`Hello. I'm a software developer who loves creating joyful products
   that make life a little nicer for everyone.`}
         </p>
+        <br />
         <p className={styles.bio}>
           {`I've worked as a full stack and mobile developer for the past 6
   years, making products in the music, medical, and energy
   industries.`}
         </p>
         <div className={styles.bottomContent}>
-          <p>
-            <b>Dear Visitor,</b>
-          </p>
-          <p>
-            This page launched December 9, 2023. There are a few things I still
-            need to add.
-          </p>
-          <p>
-            {`If you're curious, check out the current `}
-            <Link
-              className={styles.githubLink}
-              href="https://github.com/nazarioricardo/nazarioricardo/issues"
-              target="_blank"
-            >
-              github issues
-            </Link>
-            .
-          </p>
+          <div>
+            <p>
+              <b>Dear Visitor,</b>
+            </p>
+            <br />
+
+            <p>
+              This page launched December 9, 2023. There are a few things I
+              still need to add.
+            </p>
+            <br />
+
+            <p>
+              {`If you're curious, check out the current `}
+              <Link
+                className={styles.githubLink}
+                href="https://github.com/nazarioricardo/nazarioricardo/issues"
+                target="_blank"
+              >
+                github issues
+              </Link>
+              .
+            </p>
+          </div>
           <LinkButton
             text="View C.V."
             href="/Ricardo Nazario Resume.pdf"
