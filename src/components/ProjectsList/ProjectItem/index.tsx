2b271a3afd7e5879860e43b23701e14127626935
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-14 16:30:14 -0400

    Made projects look like cards

--- src/components/ProjectsList/ProjectItem/index.tsx
+++ src/components/ProjectsList/ProjectItem/index.tsx
@@ -14,52 +14,29 @@ function ProjectItem({ image, link, title, description }: ProjectItemProps) {
   const [didLoadImage, setDidLoadImage] = useState(false);
   const alt = `Image for ${title}`;
   return (
-    <>
-      <Link className={styles.project} href={link}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={`${styles.image} ${didLoadImage && styles.loadedImage}`}
-            src={image}
-            width={174}
-            height={174}
-            alt={alt}
-            onLoad={(event: SyntheticEvent<HTMLImageElement, Event>) => {
-              const { currentTarget } = event;
+    <Link className={styles.project} href={link}>
+      <div className={styles.imageContainer}>
+        <Image
+          className={`${styles.image} ${didLoadImage && styles.loadedImage}`}
+          src={image}
+          width={174}
+          height={174}
+          alt={alt}
+          onLoad={(event: SyntheticEvent<HTMLImageElement, Event>) => {
+            const { currentTarget } = event;
 
-              // next/image use an 1x1 px git as placeholder. We only want the onLoad event on the actual image
-              if (currentTarget.src.indexOf("data:image/gif;base64") < 0) {
-                setDidLoadImage(true);
-              }
-            }}
-          />
-        </div>
-        <div className={styles.content}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
-      </Link>
-      <Link className={styles.mobileProject} href={link}>
-        <div className={styles.mobileHeader}>
-          <Image
-            className={`${styles.image} ${didLoadImage && styles.loadedImage}`}
-            width={96}
-            height={96}
-            src={image}
-            alt={alt}
-            onLoad={(event: SyntheticEvent<HTMLImageElement, Event>) => {
-              const { currentTarget } = event;
-
-              // next/image use an 1x1 px git as placeholder. We only want the onLoad event on the actual image
-              if (currentTarget.src.indexOf("data:image/gif;base64") < 0) {
-                setDidLoadImage(true);
-              }
-            }}
-          />
-          <h3>{title}</h3>
-        </div>
+            // next/image use an 1x1 px git as placeholder. We only want the onLoad event on the actual image
+            if (currentTarget.src.indexOf("data:image/gif;base64") < 0) {
+              setDidLoadImage(true);
+            }
+          }}
+        />
+      </div>
+      <div className={styles.content}>
+        <h3>{title}</h3>
         <p>{description}</p>
-      </Link>
-    </>
+      </div>
+    </Link>
   );
 }
 
