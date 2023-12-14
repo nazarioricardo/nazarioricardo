4ab8b160e86bd5052ddee8706d58bd874bbca962
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-14 16:35:21 -0400

    Improved mobile styling (#10)
    * Home card
    
    * Made projects look like cards
    
    * Cleaned up project cards

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,65 +10,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main>
-        <div className={styles.content}>
-          <HomeCard>
-            <div>
-              <div className={styles.title}>
-                <h1 className={styles.name}>Ricardo Nazario</h1>
-                <p className={styles.email}>nazarioricardo@gmail.com</p>
-              </div>
-
-              <p className={styles.bio}>
-                {`Hello. I'm a software developer who loves creating joyful products
-              that make life a little nicer for everyone.`}
-              </p>
-              <p className={styles.bio}>
-                {`I've worked as a full stack and mobile developer for the past 6
-              years, making products in the music, medical, and energy
-              industries.`}
-              </p>
-              <p>
-                <b>Dear Visitor,</b>
-              </p>
-              <p>
-                This page launched December 9, 2023. There are a few things I
-                still need to add.
-              </p>
-              <p>
-                {`If you're curious, check out the current `}
-                <Link
-                  className={styles.githubLink}
-                  href="https://github.com/nazarioricardo/nazarioricardo/issues"
-                  target="_blank"
-                >
-                  github issues
-                </Link>
-                .
-              </p>
-            </div>
-            <div className={styles.cardCvButton}>
-              <LinkButton
-                text="View C.V."
-                href="/Ricardo Nazario Resume.pdf"
-                Icon={Download}
-              />
-            </div>
-          </HomeCard>
-
-          <section className={`${styles.title} ${styles.mobileTitle}`}>
-            <h1 className={styles.name}>Ricardo Nazario</h1>
-            <p className={styles.email}>nazarioricardo@gmail.com</p>
-            <div className={styles.mobileCvButton}>
-              <Link className={styles.link} href="/Ricardo Nazario Resume.pdf">
-                <span> View C.V. </span>
-                <Download />
-              </Link>
-            </div>
-          </section>
-
-          <ProjectList />
-        </div>
+      <main className={styles.content}>
+        <HomeCard />
+        <ProjectList />
       </main>
     </>
   );
--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -1,60 +1,53 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
-import Sheet, { SheetRef } from "react-modal-sheet";
+import { Download } from "@mui/icons-material";
+import LinkButton from "@nr/components/LinkButton";
+import Link from "next/link";
 import styles from "./styles.module.css";
 
-type HomeCardProps = {
-  children: ReactNode;
-};
-
-function HomeCard({ children }: HomeCardProps) {
-  const [shouldDisplay, setShouldDisplay] = useState(false);
-  const [isActive, setIsActive] = useState(true);
-
-  const sheetRef = useRef<SheetRef>();
-
-  const onSnap = (snapIndex: number) => {
-    if (snapIndex === 0) {
-      setIsActive(true);
-    } else {
-      setIsActive(false);
-    }
-  };
-
-  const snap = () => {
-    if (isActive) {
-      sheetRef.current?.snapTo(1);
-    } else {
-      sheetRef.current?.snapTo(0);
-    }
-  };
-
-  useEffect(() => {
-    setShouldDisplay(true);
-  }, []);
-
+function HomeCard() {
   return (
-    <>
-      <div className={styles.homeCard}>{children}</div>
+    <section className={styles.homeCard}>
+      <div className={styles.cardContent}>
+        <div className={styles.title}>
+          <h1 className={styles.name}>Ricardo Nazario</h1>
+          <p className={styles.email}>nazarioricardo@gmail.com</p>
+        </div>
 
-      <Sheet
-        className={styles.bottomSheet}
-        ref={sheetRef}
-        isOpen={shouldDisplay}
-        onClose={() => console.log("Did close, that would be bad.")}
-        snapPoints={[-96, 96]}
-        initialSnap={1}
-        disableScrollLocking={!isActive}
-        onSnap={onSnap}
-      >
-        <Sheet.Container
-          className={styles.bottomSheetContainer}
-          style={{ borderTopRightRadius: 96, borderTopLeftRadius: 96 }}
-        >
-          <Sheet.Header onTap={snap} style={{ paddingBottom: 96 }} />
-          <Sheet.Content>{children}</Sheet.Content>
-        </Sheet.Container>
-      </Sheet>
-    </>
+        <p className={styles.bio}>
+          {`Hello. I'm a software developer who loves creating joyful products
+  that make life a little nicer for everyone.`}
+        </p>
+        <p className={styles.bio}>
+          {`I've worked as a full stack and mobile developer for the past 6
+  years, making products in the music, medical, and energy
+  industries.`}
+        </p>
+        <div className={styles.bottomContent}>
+          <p>
+            <b>Dear Visitor,</b>
+          </p>
+          <p>
+            This page launched December 9, 2023. There are a few things I still
+            need to add.
+          </p>
+          <p>
+            {`If you're curious, check out the current `}
+            <Link
+              className={styles.githubLink}
+              href="https://github.com/nazarioricardo/nazarioricardo/issues"
+              target="_blank"
+            >
+              github issues
+            </Link>
+            .
+          </p>
+          <LinkButton
+            text="View C.V."
+            href="/Ricardo Nazario Resume.pdf"
+            Icon={Download}
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
