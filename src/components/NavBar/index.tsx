634078013b87bb9ee9e754402fbe98e706694fe2
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-17 12:50:21 -0400

    Nice animation

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ import styles from "./styles.module.css";
 function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <Link className={styles.logo} href="/">
+      <Link href="/">
         <R height={48} width={48} />
       </Link>
 
