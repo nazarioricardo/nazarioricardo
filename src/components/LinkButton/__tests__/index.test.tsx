93fc5529366048aa1ba171ac989a818b386f2c87
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-20 16:24:58 -0400

    Passes

---
+++ src/components/LinkButton/__tests__/index.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LinkButton from "../index"; // adjust this import according to your file structure
+import { Home } from "@mui/icons-material";
+
+describe("LinkButton", () => {
+  test("renders LinkButton component", () => {
+    render(<LinkButton text="Home" Icon={Home} href="/home" />);
+    const linkElement = screen.getByRole("link", { name: /Home/ });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", "/home");
+  });
+
+  test("renders LinkButton component with icon", () => {
+    render(<LinkButton text="Home" Icon={Home} href="/home" />);
+    const iconElement = screen.getByTestId("HomeIcon");
+    expect(iconElement).toBeInTheDocument();
+  });
+
+  test("navigates to the correct page when link is clicked", () => {
+    render(<LinkButton text="Home" Icon={Home} href="/home" />);
+    const linkElement = screen.getByRole("link", { name: /Home/ });
+    fireEvent.click(linkElement);
+    // Add assertion here to check that the correct page was navigated to
+    // This will depend on your routing setup and what the link is supposed to do
+  });
+
+  test("matches snapshot", () => {
+    const { asFragment } = render(
+      <LinkButton text="Home" Icon={Home} href="/home" />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  // Add more tests as needed
+});
