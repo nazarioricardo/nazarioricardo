7b1881f9a25d531b3a05d16f62fed43b652cf414
Ricardo Nazario <nazarioricardo@gmail.com>
2023-12-20 16:45:59 -0400

    Test

---
+++ src/components/ImageFadeIn/__tests__/index.test.tsx
@@ -0,0 +1,26 @@
+// ImageFadeIn.test.tsx
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ImageFadeIn from "../index"; // adjust this import according to your file structure
+
+describe("ImageFadeIn", () => {
+  const SRC = "/path/to/image.jpg";
+  test("renders ImageFadeIn component", () => {
+    render(<ImageFadeIn src={SRC} alt="Test Image" priority />);
+    const imgElement = screen.getByAltText("Test Image") as HTMLImageElement;
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement.src).toMatch(
+      /\/_next\/image\?url=%2Fpath%2Fto%2Fimage\.jpg/
+    );
+  });
+
+  test("matches snapshot", () => {
+    const { asFragment } = render(
+      <ImageFadeIn src={SRC} alt="Test Image" priority />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  // Add more tests as needed
+});
